202ef7d071c404680b952464c0f42f61d14528d0
Gulnoza Muminova <gmcharmy@gmail.com>
2019-08-12 14:54:23 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -16,8 +17,35 @@ class ArticlesController < ApplicationController
     @article.title = params[:title]
     @article.description = params[:description]
     @article.save
-    redirect_to article_path(@article)
+    redirect_to @article
   end
 
+  def edit
+  end
+
+  def update
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to @article
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles
+  end
+
+private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+   params.require(:article).permit(:title, :description)
+  end
+  
   # add edit and update methods here
 end
+
+# @article = Article.find(params[:id])
+#     @article.update(title: params[:article][:title], description: params[:article][:description])
+#     redirect_to article_path(@article)
